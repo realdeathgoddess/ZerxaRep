8a07791d80b8f833df9eef72d21d9e72e17cbf61
Zerxa Aeon Mamac <zerxa_mamac@umindanao.edu.ph>
2017-02-02 11:08:58 +0000

    This yow

---
+++ TextFieldValue.php
@@ -0,0 +1,14 @@
+<HTML>
+<BODY>
+<CENTER><?php echo date("l, F j, Y") ?></CENTER>
+<HR>
+<?php echo $CITY ?>:
+<P>
+<?php echo $STAR ?>    <br>
+<?php echo $FLAVOR ?>  <br>
+<?php echo $RIVER ?>   <br>
+<?php echo $CITY ?>    <br>
+<?php echo $USER ?>    <br>
+
+</BODY>
+</HTML>
--- zerxa.php
+++ zerxa.php
@@ -1,2 +1,40 @@
-<?php
 
+<HTML>
+<BODY>
+<FORM METHOD="POST" ACTION="TextFieldValue">
+    <TABLE>
+    
+    <TR>
+    <TD ALIGN="RIGHT">User's First Name:</TD>
+    <TD><INPUT TYPE="TEXT" NAME="USER"></TD>
+    </TR>
+    
+    <TR>
+    <TD ALIGN="RIGHT">Major City:</TD>
+    <TD><INPUT TYPE="TEXT" NAME="CITY"></TD>
+    </TR>
+    
+    <TR>
+    <TD ALIGN="RIGHT">Favorite Ice Cream Flavor:</TD>
+    <TD><INPUT TYPE="TEXT" NAME="FLAVOR"></TD>
+    </TR>
+    
+    <TR>
+    <TD ALIGN="RIGHT">Favorite Pop Music Star:</TD>
+    <TD><INPUT TYPE="TEXT" NAME="STAR"></TD>
+    </TR>
+    
+    <TR>
+    <TD ALIGN="RIGHT">Name of Famous River:</TD>
+    <TD><INPUT TYPE="TEXT" NAME="RIVER"></TD>
+    </TR>
+    
+    <TR>
+    <TD></TD>
+    <TD><INPUT TYPE="SUBMIT"></TD>
+    </TR>
+    
+    </TABLE>
+</FORM>
+</BODY>
+</HTML>
